43205317d1ad00bf18ac496099c775833d4d9d00
hkgumbs <kofi.gumbs@raise.com>
2016-08-12 20:57:28 -0600

    Update README and setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-chsh -s $(which zsh)
+### GIT ###
 git config --global core.editor $(which vim)
 git config --global user.name "hkgumbs"
+###########
+
+### ZSH ###
+chsh -s $(which zsh)
+ln -s zshrc ~/.zshrc
+###########
+
+### VIM ###
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s vimrc ~/.vimrc
+vim +PlugInstall +qall
+###########
