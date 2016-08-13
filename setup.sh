042f265ea0d3d28d9803c2228fec606aa6f53221
hkgumbs <kofi.gumbs@raise.com>
2016-08-12 21:07:17 -0600

    Fix Plug install (oops, it was Vundle still)

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ ln -s zshrc ~/.zshrc
 ###########
 
 ### VIM ###
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s vimrc ~/.vimrc
 vim +PlugInstall +qall
 ###########
